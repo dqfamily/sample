b4590cebc2404948bc8522bc62b4db693b78395f
dqfamily <yanluping2009@outlook.com>
2025-09-28 17:34:16 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "bits/stdc++.h"
 #include "windows.h"
 
 using namespace std;
@@ -6,7 +7,8 @@ using namespace std;
 int main(){
     // 设置终端使用UTF-8编码
     SetConsoleOutputCP(65001);
+    cout << "(c) 2025 衡南一中编程社\nAll riights reserved." << endl;
     
-    cout << "Hello, from sample!" << endl;
-    cout << "乐" << endl;
+    // 写下你的代码
+
 }
